735b2b773074dc4895668727464aad6fcd8c4988
fatiya17 <fatiyalabibah17@gmail.com>
2026-07-30 15:02:02 +0700

    feat(ui): create custom textarea component

--- components/ui/textarea.jsx
+++ components/ui/textarea.jsx
@@ -1,16 +1,18 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
+// kolom ketik teks panjang beberapa baris (seperti pesan, catatan, atau detail laporan)
 function Textarea({
   className,
+  spellCheck = false,
   ...props
 }) {
   return (
     <textarea
       data-slot="textarea"
+      spellCheck={spellCheck}
       className={cn(
-        "flex field-sizing-content min-h-16 w-full rounded-lg border border-input bg-transparent px-2.5 py-2 text-base transition-colors outline-none placeholder:text-sm placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
+        "flex min-h-24 w-full rounded-xl border border-slate-200 dark:border-slate-800 bg-transparent px-4 py-3 text-xs md:text-sm text-slate-800 dark:text-slate-200 transition-colors outline-none placeholder:text-slate-450 placeholder:font-light focus-visible:border-primary focus-visible:ring-1 focus-visible:ring-primary disabled:cursor-not-allowed disabled:bg-slate-50/50 dark:disabled:bg-slate-900/50 disabled:opacity-50 aria-invalid:border-[1.5px] aria-invalid:border-destructive aria-invalid:focus-visible:border-slate-350 dark:aria-invalid:focus-visible:border-slate-700 aria-invalid:focus-visible:ring-0 resize-none overflow-hidden",
         className
       )}
       {...props} />
