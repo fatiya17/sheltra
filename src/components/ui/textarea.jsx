26d7e94d317e18c4d775729ca9b5924b64ca0df8
fatiya17 <fatiyalabibah17@gmail.com>
2026-08-02 21:23:22 +0700

    style: update textarea component for incident description

--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-// kolom ketik teks panjang beberapa baris (seperti pesan, catatan, atau detail laporan)
+// input textarea multiline
 function Textarea({
   className,
   spellCheck = false,
@@ -12,7 +12,7 @@ function Textarea({
       data-slot="textarea"
       spellCheck={spellCheck}
       className={cn(
-        "flex min-h-24 w-full rounded-xl border border-slate-200 dark:border-slate-800 bg-transparent px-4 py-3 text-xs md:text-sm text-slate-800 dark:text-slate-200 transition-colors outline-none placeholder:text-slate-450 placeholder:font-light focus-visible:border-primary focus-visible:ring-1 focus-visible:ring-primary disabled:cursor-not-allowed disabled:bg-slate-50/50 dark:disabled:bg-slate-900/50 disabled:opacity-50 aria-invalid:border-[1.5px] aria-invalid:border-destructive aria-invalid:focus-visible:border-slate-350 dark:aria-invalid:focus-visible:border-slate-700 aria-invalid:focus-visible:ring-0 resize-none overflow-hidden",
+        "flex min-h-24 w-full rounded-xl border border-slate-200 dark:border-slate-800 bg-transparent px-4 py-3 text-sm text-slate-800 dark:text-slate-200 transition-colors outline-none placeholder:text-slate-450 placeholder:font-light focus:outline-none focus:border-black focus:ring-1 focus:ring-black dark:focus:border-white dark:focus:ring-white disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-slate-50/50 dark:disabled:bg-slate-900/50 disabled:opacity-50 aria-invalid:border-red-500 aria-invalid:focus:border-black aria-invalid:focus:ring-black dark:aria-invalid:focus:border-white dark:aria-invalid:focus:ring-white resize-none overflow-hidden",
         className
       )}
       {...props} />
