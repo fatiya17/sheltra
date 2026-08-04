704810f8dc02be61efc5a70d23e20091ece62b8c
fatiya17 <fatiyalabibah17@gmail.com>
2026-08-04 22:55:46 +0700

    feat(sos): create page main for sos

---
+++ src/app/(public)/sos/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import React from "react"
+import SosContainer from "@/features/sos/components/sos-container"
+
+export default function SosPage() {
+  return (
+    <main className="min-h-screen w-full bg-background flex items-center justify-center">
+      <div className="w-full flex items-center justify-center">
+        <SosContainer />
+      </div>
+    </main>
+  )
+}
